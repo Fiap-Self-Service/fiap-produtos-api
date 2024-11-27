55a4a7b115ed08e8f3115d89330d3d61e1e1ec10
Wanderson Pereira <wanderson.p.ayres@gmail.com>
2024-11-27 14:31:57 -0300

    feat/criando a validação de categoria para o cadastro do produto

--- src/core/use-cases/cadastrar-produto-use-case.ts
+++ src/core/use-cases/cadastrar-produto-use-case.ts
@@ -1,11 +1,17 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { ProdutoGateway } from "../adapters/gateways/produto-gateway";
 import { ProdutoDTO } from "../dto/produtoDTO";
 import { Produto } from "../entities/produto";
+import { CategoriaProdutoType } from "../dto/categoria-produto-type-enum";
 
 @Injectable()
 export class CadastrarProdutoUseCase {
   async execute(produtoGateway: ProdutoGateway, produtoDTO: ProdutoDTO): Promise<Produto> {
+
+    // Verifica se a categoria é válida
+    if (!Object.values(CategoriaProdutoType).includes(produtoDTO.categoria as CategoriaProdutoType)) {
+      throw new HttpException('Categoria inválida.', HttpStatus.BAD_REQUEST);
+    }
 
     const produto = new Produto(produtoDTO.nome, produtoDTO.descricao, produtoDTO.categoria, produtoDTO.valor);
 
