f460e8b9fa599be5f43ca06a271b19e5b8613d7f
Wanderson Pereira <wanderson.p.ayres@gmail.com>
2024-11-27 11:16:41 -0300

    fix/ajuste para tratar consulta de produto por categoria inexistente

--- src/core/adapters/controllers/buscar-produto-por-categoria-controller.ts
+++ src/core/adapters/controllers/buscar-produto-por-categoria-controller.ts
@@ -2,6 +2,7 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { ProdutoDTO } from '../../dto/produtoDTO';
 import { ProdutoGateway } from '../gateways/produto-gateway';
 import { BuscarProdutoPorCategoriaUseCase } from '../../use-cases/buscar-produto-por-categoria-use-case';
+import { CategoriaProdutoType } from '../../dto/categoria-produto-type-enum';
 
 @Injectable()
 export class BuscarProdutoPorCategoriaController {
@@ -12,6 +13,11 @@ export class BuscarProdutoPorCategoriaController {
   ) {}
 
   async execute(categoriaId: string): Promise<ProdutoDTO[]> {
+
+    if (!Object.values(CategoriaProdutoType).includes(categoriaId as CategoriaProdutoType)) {
+      throw new HttpException('Categoria não encontrada.', HttpStatus.BAD_REQUEST);
+    }
+
     const produtos = await this.produtoGateway.buscarProdutoPorCategoria(categoriaId);
 
     if (!produtos || produtos.length === 0) {
